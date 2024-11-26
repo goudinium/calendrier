fd76cac2f3dc6b02dfa80e249e7f3ebe16d34873
François Goudineau <francoisgoudineau@MacBook-Pro-de-Francois.local>
2024-11-27 00:47:11 +0100

    Ajout de tests automatiques

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,52 @@
 import pytest 
-from petitcalendrier import app as calendar_app
+from petitcalendrier import create_app, db, bcrypt
+from petitcalendrier.models import User, Question
+
+class TestConfig():
+    SECRET_KEY = '74f98e76805cca74c37072e4f2e715af'
+    SQLALCHEMY_DATABASE_URI = 'sqlite://' 
+    TESTING = True
+    WTF_CSRF_ENABLED = False
+    DAY = 10
+
+class AuthActions(object):
+    def __init__(self, client):
+        self._client = client
+
+    def login(self, username='admin', password='fanfan'):
+        return self._client.post(
+            '/login',
+            data={'username': username, 'password': password, 'remember':'y', 'submit': 'Connexion'},
+            follow_redirects=True
+        )
+
+    def logout(self):
+        return self._client.get('/logout')
 
 @pytest.fixture()
 def app():
-    yield calendar_app 
+    app = create_app(TestConfig)
+    
+    with app.app_context():
+        db.create_all()
+        # creating one admin in the db
+        pwd = bcrypt.generate_password_hash("fanfan").decode('utf-8')
+        admin = User(username='admin', first_name="François", last_name="Goudineau", password=pwd, is_admin=True)
+        db.session.add(admin)
+        db.session.commit()
+
+        # creating fake questions
+        for day in range(1, 25):
+            q = Question(day=day, image=f"img-{day}", answer=f"answer-{day}", solution_time="", solution_object="", solution_place="", solution_sound="", solution_color="")
+            db.session.add(q)
+        db.session.commit()
+        
+    yield app 
 
 @pytest.fixture()
 def client(app):
     return app.test_client()
+
+@pytest.fixture
+def auth(client):
+    return AuthActions(client)
--- tests/test_calendar.py
+++ tests/test_calendar.py
@@ -1,4 +1,25 @@
+from flask import url_for
+
+def test_app_is_under_testing_mode(app):
+    assert app.testing 
+
 def test_user_is_redirected_to_login_page_if_not_connected(client):
     response = client.get("/", follow_redirects=True)
     assert response.request.path == "/login"
 
+def test_login(client, auth):
+    assert client.get("/login").status_code == 200
+    response = auth.login()
+    assert response.request.path == "/calendar"
+    assert b"Bonjour" in response.data
+
+def test_user_cannot_access_future_calendar_days(client, auth):
+    auth.login()
+    with client:
+        response = client.get("/day/15", follow_redirects=True)
+        assert response.request.path == "/calendar"
+
+def test_user_cannot_access_days_after_24th_december(client, auth):
+    auth.login()
+    with client:
+        assert client.get("/day/25", follow_redirects=True).status_code == 404
