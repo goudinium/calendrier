7fce55a4a2c3140cdebf4961009e028d95cac7c7
Francois Goudineau <francois.goudineau@gmail.com>
2024-12-01 10:27:51 +0100

    Ajout d'une sécurité sur la longueur des réponses

--- petitcalendrier/calendar/forms.py
+++ petitcalendrier/calendar/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 class AnswerForm(FlaskForm):
     answer_character = SelectField("🙍 Quelle personne se cache derrière cette image?", choices=[
@@ -13,10 +13,10 @@ class AnswerForm(FlaskForm):
         ('Quentin', 'Quentin'), ('Soraya', 'Soraya'), ('Stephane', 'Stéphane'), 
         ('Theophile', 'Théophile'), ('Tiphaine', 'Tiphaine'), ('Tristan', 'Tristan'), 
         ('Valerie', 'Valérie')], validators=[DataRequired(True)])
-    answer_time = StringField("📅 Quel est le moment caché?", validators=[DataRequired()])
-    answer_place = StringField("🌍 Quel est le lieu caché?", validators=[DataRequired()])
-    answer_color = StringField("🎨 Quelle est la couleur cachée?", validators=[DataRequired()])
-    answer_sound = StringField("🔊 Quel est le bruit caché?", validators=[DataRequired()])
-    answer_object = StringField("💎Quel est l'objet vintage caché?", validators=[DataRequired()])
+    answer_time = StringField("📅 Quel est le moment caché?", validators=[DataRequired(), Length(min=1, max=60, message="Réponse trop longue/vide")])
+    answer_place = StringField("🌍 Quel est le lieu caché?", validators=[DataRequired(), Length(min=1, max=60, message="Réponse trop longue/vide")])
+    answer_color = StringField("🎨 Quelle est la couleur cachée?", validators=[DataRequired(), Length(min=1, max=60, message="Réponse trop longue/vide")])
+    answer_sound = StringField("🔊 Quel est le bruit caché?", validators=[DataRequired(), Length(min=1, max=60, message="Réponse trop longue/vide")])
+    answer_object = StringField("💎Quel est l'objet vintage caché?", validators=[DataRequired(), Length(min=1, max=60, message="Réponse trop longue/vide")])
     submit = SubmitField("Valider")
 
